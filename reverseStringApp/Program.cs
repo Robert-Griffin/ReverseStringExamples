9a2f13547502bdb66fdfe6723a270b74fc604acf
Robert Griffin <Robert.griffin0908@gmail.com>
2022-11-12 12:37:15 -0500

    chore: reset project structure to semantically add features

---
+++ reverseStringApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reverseStringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
