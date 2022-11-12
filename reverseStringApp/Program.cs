4e8c3c6c18134a51d2bf9b70fa3934724570d6fd
Robert Griffin <Robert.griffin0908@gmail.com>
2022-11-12 12:56:59 -0500

    feat: added method to reverse string using BuiltIns

--- reverseStringApp/Program.cs
+++ reverseStringApp/Program.cs
@@ -6,7 +6,30 @@ namespace reverseStringApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Enter a string to be reversed");
+
+            string inputString = Console.ReadLine();
+
+
+            ReverseStringMethod(inputString);
+
+            string ReverseStringMethod(string s)
+            {
+
+                Console.WriteLine("Reverse by converting to Array and using builtin Array.Reverse()");
+
+                char[] array = s.ToCharArray();
+
+                Array.Reverse(array);
+
+                string result = new string(array);
+
+
+                Console.WriteLine(result);
+                return result;
+
+            }
+
         }
     }
 }
